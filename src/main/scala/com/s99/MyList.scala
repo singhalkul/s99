f737ce7b0399e93796c5feef3a68c393df1bbc51
Kul <ksinghal@equalexperts.com>
2015-04-14 22:51:07 +0530

    implemented problem 9. refactored code to convert MyList into List implicitly

--- src/main/scala/com/s99/MyList.scala
+++ src/main/scala/com/s99/MyList.scala
@@ -1,18 +1,31 @@
 package com.s99
 
-/**
- * @author: Kul.
- */
-class MyList {
+import MyList._
 
-  def compress[A](list: List[A]): List[A] = {
+/**
+ * author: Kul
+ */
+class MyList[A](self: List[A]) {
+
+  def pack(): List[List[A]] = {
+    if (self isEmpty) List(Nil)
+    else {
+      val (packed, rest) = self.mySpan(a => a == self.head)
+      (packed, rest) match {
+        case (p, Nil) => List(p)
+        case (p, r) => p :: r.pack()
+      }
+    }
+  }
+
+  def compress(): List[A] = {
     def compressR(list: List[A], result: List[A]): List[A] = (list, result) match {
       case (Nil, res) => res
-      case(h :: tail, Nil) => compressR(tail, h :: Nil)
+      case (h :: tail, Nil) => compressR(tail, h :: Nil)
       case (h :: tail, r :: resTail) if h == r => compressR(tail, result)
       case (h :: tail, r :: resTail) => compressR(tail, h :: result)
     }
-    reverse(compressR(list, Nil))
+    compressR(self, Nil).myReverse
   }
 
   private def flattenTailRecursive[A](result: List[A], input: List[A], remainingTail: List[A]): List[A] = input match {
@@ -24,55 +37,81 @@ class MyList {
   }
 
   private def flattenNonTailRecursive[A](list: List[A]): List[A] = list match {
-      case Nil => Nil
-      case (l : List[A]) :: tail => flattenNonTailRecursive(l) ++ flattenNonTailRecursive(tail)
-      case h :: tail => h :: flattenNonTailRecursive(tail)
-    }
+    case Nil => Nil
+    case (l: List[A]) :: tail => flattenNonTailRecursive(l) ++ flattenNonTailRecursive(tail)
+    case h :: tail => h :: flattenNonTailRecursive(tail)
+  }
 
-  def flatten[A](list: List[A]): List[A] = reverse(flattenTailRecursive(Nil, list, Nil))
+  def myFlatten(): List[A] = flattenTailRecursive(Nil, self, Nil).myReverse()
 
-  def isPalindrome[A](list: List[A]) = {
+  def isPalindrome() = {
     def isPalindromeR(list: List[A], reversed: List[A]): Boolean = (list, reversed) match {
       case (Nil, Nil) => true
       case (h1 :: tail1, h2 :: tail2) => h1 == h2 && isPalindromeR(tail1, tail2)
     }
-    isPalindromeR(list, reverse(list))
+    isPalindromeR(self, self.myReverse)
   }
 
-  def length[A](list: List[A]): Int = {
+  def myLength(): Int = {
     def tailLength(list: List[A], result: Int): Int = list match {
       case Nil => result
       case h :: tail => tailLength(tail, result + 1)
     }
-    tailLength(list, 0)
+    tailLength(self, 0)
   }
 
-  def reverse[A](list: List[A]): List[A] = {
+  def myReverse(): List[A] = {
     def tailReverse(input: List[A], result: List[A]): List[A] = input match {
       case Nil => result
       case h :: tail => tailReverse(tail, h :: result)
     }
-    tailReverse(list, Nil)
+    tailReverse(self, Nil)
   }
 
-  def kth[A](list: List[A], k: Int): Option[A] = (k, list) match {
+  def kth(k: Int): Option[A] = (k, self) match {
     case (p, Nil) => None
     case (0, h :: tail) => Some(h)
-    case (n, h :: tail) => kth(tail, n - 1)
+    case (n, h :: tail) => tail.kth(n - 1)
   }
 
-  def penultimate[A](list: List[A]): Option[A] = list match {
+  def penultimate(): Option[A] = self match {
     case a :: b :: Nil => Some(a)
-    case a :: l => penultimate(l)
-    case a :: Nil => None
+    case a :: tail => tail.penultimate()
     case Nil => None
   }
 
-  def last[A](list: List[A]): Option[A] = {
-    list match {
-      case Nil => None
-      case a :: Nil => Some(a)
-      case a :: l => last(l)
-    }
+  def myLast(): Option[A] = self match {
+    case Nil => None
+    case a :: Nil => Some(a)
+    case a :: tail => tail.myLast()
   }
+
+  def mySpan(f: A => Boolean) = (self.myTakeWhile(f), self.myDropWhile(f))
+
+  def myTakeWhile(f: A => Boolean) = {
+    def takeWhileR(f: A => Boolean)(list: List[A], result: List[A]): List[A] = list match {
+      case Nil => result
+      case head :: tail if f(head) => takeWhileR(f)(tail, head :: result)
+      case head :: tail => result
+    }
+    takeWhileR(f)(self, Nil)
+  }
+
+  def myDropWhile(f: A => Boolean): List[A] = self match {
+    case Nil => Nil
+    case head :: (tail) if f(head) => tail.myDropWhile(f)
+    case head :: tail => head :: tail
+  }
+
+  def myMap[B](f: A => B): List[B] = {
+    def myMapR(f: A => B, list: List[A], result: List[B]): List[B] = list match {
+      case Nil => result
+      case head :: tail => myMapR(f, tail, f(head) :: result)
+    }
+    myMapR(f, self, Nil).reverse
+  }
+}
+
+object MyList {
+  implicit def enhancedList[T](self: List[T]): MyList[T] = new MyList(self)
 }
--- src/test/scala/com/s99/MyListTest.scala
+++ src/test/scala/com/s99/MyListTest.scala
@@ -1,167 +1,252 @@
 package com.s99
 
+import MyList._
 import org.scalatest.{Matchers, FunSpec}
 
 /**
- * @author: Kul.
+ * author: Kul.
  */
 class MyListTest extends FunSpec with Matchers {
 
-  val myList = new MyList
+  val myList = new MyList(Nil)
 
   describe("finding the last element of list") {
     it("should return last element from a list") {
-      myList.last(List(1, 2, 3, 4, 5)) should be(Some(5))
+      List(1, 2, 3, 4, 5).myLast() should be(Some(5))
     }
 
     it("should return None") {
-      myList.last(Nil) should be(None)
+      Nil.myLast() should be(None)
     }
   }
 
   describe("finding penultimate element of list") {
     it("should return None if list is empty") {
-      myList.penultimate(Nil) should be(None)
+      Nil.penultimate() should be(None)
     }
 
     it("should return None of list size is 1") {
-      myList.penultimate(List(1)) should be(None)
+      List(1).penultimate() should be(None)
     }
 
     it("should return the penultimate element from a list of size of 2 elements") {
-      myList.penultimate(List(2, 3)) should be(Some(2))
+      List(2, 3).penultimate() should be(Some(2))
     }
 
     it("should return the penultimate element from a list of size of 3 elements") {
-      myList.penultimate(List(1, 2, 3)) should be(Some(2))
+      List(1, 2, 3).penultimate() should be(Some(2))
     }
 
     it("should return the penultimate element from a list of size greater than 3 elements") {
-      myList.penultimate(List(1, 2, 3, 4, 5)) should be(Some(4))
+      List(1, 2, 3, 4, 5).penultimate() should be(Some(4))
     }
   }
 
   describe("finding kth element of list") {
     it("should return None if list is empty") {
-      myList.kth(Nil, 1) should be(None)
+      Nil.kth(1) should be(None)
     }
 
     it("should return the kth for list of size 1") {
-      myList.kth(List(1), 0) should be(Some(1))
+      List(1).kth(0) should be(Some(1))
     }
 
     it("should return the 1st for list of size 2") {
-      myList.kth(List(1, 2), 0) should be(Some(1))
+      List(1, 2).kth(0) should be(Some(1))
     }
 
     it("should return the 2nd for list of size 2") {
-      myList.kth(List(1, 2), 1) should be(Some(2))
+      List(1, 2).kth(1) should be(Some(2))
     }
 
     it("should return the 4th for list of size 6") {
-      myList.kth(List(1, 2, 3, 4, 5, 6), 3) should be(Some(4))
+      List(1, 2, 3, 4, 5, 6).kth(3) should be(Some(4))
     }
   }
 
   describe("reversing a list should reverse") {
     it("Nil for empty list") {
-      myList.reverse(Nil) should be(Nil)
+      Nil.myReverse() should be(Nil)
     }
 
     it("list of size 1") {
-      myList.reverse(List(1)) should be(List(1))
+      List(1).myReverse() should be(List(1))
     }
 
     it("list of size 2") {
-      myList.reverse(List(1, 2)) should be(List(2, 1))
+      List(1, 2).myReverse() should be(List(2, 1))
     }
 
     it("list of size greater than 2") {
-      myList.reverse(List(1, 2, 3, 4, 5)) should be(List(5, 4, 3, 2, 1))
+      List(1, 2, 3, 4, 5).myReverse() should be(List(5, 4, 3, 2, 1))
     }
   }
 
   describe("finding length of list") {
     it("should return 0 for empty list") {
-      myList.length(Nil) should be(0)
+      Nil.myLength() should be(0)
     }
 
     it("should return 1 for list of size 1") {
-      myList.length(List(1)) should be(1)
+      List(1).myLength() should be(1)
     }
 
     it("should return 2 for list of size 2") {
-      myList.length(List(1, 2)) should be(2)
+      List(1, 2).myLength() should be(2)
     }
 
     it("should return 5 for list of size 5") {
-      myList.length(List(1, 2, 3, 4, 5)) should be(5)
+      List(1, 2, 3, 4, 5).myLength() should be(5)
     }
   }
 
   describe("checking whether a list is palindrome") {
     it("should return true for empty list") {
-      myList.isPalindrome(Nil) should be(right = true)
+      Nil.isPalindrome() should be(right = true)
     }
 
     it("should return true for list with one element") {
-      myList.isPalindrome(List(1)) should be(right = true)
+      List(1).isPalindrome() should be(right = true)
     }
 
     it("should return false for list 1,2") {
-      myList.isPalindrome(List(1, 2)) should be(right = false)
+      List(1, 2).isPalindrome() should be(right = false)
     }
 
     it("should return true for list 1,2,1,2,1") {
-      myList.isPalindrome(List(1, 2, 1, 2, 1)) should be(right = true)
+      List(1, 2, 1, 2, 1).isPalindrome() should be(right = true)
     }
 
     it("should return false for list 1,2,3") {
-      myList.isPalindrome(List(1, 2, 3)) should be(right = false)
+      List(1, 2, 3).isPalindrome() should be(right = false)
     }
   }
 
   describe("flattening a list") {
     it("should return Nil for empty list") {
-      myList.flatten(Nil) should be(Nil)
+      Nil.myFlatten() should be(Nil)
     }
 
     it("should return same list for List(1,2,3") {
-      myList.flatten(List(1, 2, 3)) should be(List(1, 2, 3))
+      List(1, 2, 3).myFlatten() should be(List(1, 2, 3))
     }
 
     it("should return flattened list for List(1,List(2,3,4),3)") {
-      myList.flatten(List(1, List(2, 3, 4), 3)) should be(List(1, 2, 3, 4, 3))
+      List(1, List(2, 3, 4), 3).myFlatten() should be(List(1, 2, 3, 4, 3))
     }
 
     it("should return flattened list for List(List(List(2)))") {
-      myList.flatten(List(List(List(2)))) should be(List(2))
+      List(List(List(2))).myFlatten() should be(List(2))
     }
 
     it("should return flattened list for List(List(List(1,2)), 3)") {
-      myList.flatten(List(List(List(1,2)), 3)) should be(List(1,2,3))
+      List(List(List(1, 2)), 3).myFlatten() should be(List(1, 2, 3))
     }
   }
 
   describe("removing consecutive duplicates from a list") {
     it("should return Nil from empty list") {
-      myList.compress(Nil) should be(Nil)
+      Nil.compress() should be(Nil)
     }
 
     it("should return List(1) for List(1)") {
-      myList.compress(List(1)) should be(List(1))
+      List(1).compress() should be(List(1))
     }
 
     it("should return List(1) for List(1,1)") {
-      myList.compress(List(1,1)) should be(List(1))
+      List(1, 1).compress() should be(List(1))
     }
 
     it("should return List(1,2) for List(1,2)") {
-      myList.compress(List(1,2)) should be(List(1,2))
+      List(1, 2).compress() should be(List(1, 2))
     }
 
     it("should return compressed list") {
-      myList.compress(List(1,1,1,1,2,3,3,1,1,4,5,5,5,5)) should be(List(1,2,3,1,4,5))
+      List(1, 1, 1, 1, 2, 3, 3, 1, 1, 4, 5, 5, 5, 5).compress() should be(List(1, 2, 3, 1, 4, 5))
+    }
+  }
+
+  describe("packing consecutive duplicates") {
+
+    it("should return List of Nil for empty List") {
+      Nil.pack() should be(List(Nil))
+    }
+
+    it("should return List(List(1)) for List(1)") {
+      List(1).pack() should be(List(List(1)))
+    }
+
+    it("should return List(List(1), List(2)) for List(1,2)") {
+      List(1, 2).pack() should be(List(List(1), List(2)))
+    }
+
+    it("should return List(List(1,1,1), List(2,2), List(1)) for List(1,1,1,2,2,1)") {
+      List(1, 1, 1, 2, 2, 1).pack() should be(List(List(1, 1, 1), List(2, 2), List(1)))
+    }
+  }
+  describe("take while") {
+    it("should return Nil for empty list") {
+      List().myTakeWhile(_ => true) should be(Nil)
+    }
+
+    it("should return 1 for List(1) when true") {
+      List(1).myTakeWhile(_ => true) should be(List(1))
+    }
+
+    it("should return Nil for List(1) when false") {
+      List(1).myTakeWhile(_ => false) should be(Nil)
+    }
+
+    it("should return List(1) for List(1,2) when taking 1") {
+      List(1, 2).myTakeWhile(_ == 1) should be(List(1))
+    }
+
+    it("should return List(1) for List(1,2,1) when taking 1") {
+      List(1, 2, 1).myTakeWhile(_ == 1) should be(List(1))
+    }
+
+    it("should return List(1,1,1) for List(1,1,1,2,1,1) when taking 1") {
+      List(1, 1, 1, 2, 1, 1, 1).myTakeWhile(_ == 1) should be(List(1, 1, 1))
+    }
+  }
+
+  describe("drop while") {
+    it("should return Nil for empty list") {
+      Nil.myDropWhile(_ => true) should be(Nil)
+    }
+
+    it("should return 1 for List(1) when false") {
+      List(1).myDropWhile(_ => false) should be(List(1))
+    }
+
+    it("should return Nil for List(1) when true") {
+      List(1).myDropWhile(_ => true) should be(Nil)
+    }
+
+    it("should return List(2) for List(1,2) when taking 1") {
+      List(1, 2).myDropWhile(_ == 1) should be(List(2))
+    }
+
+    it("should return List(2,1) for List(1,2,1) when taking 1") {
+      List(1, 2, 1).myDropWhile(_ == 1) should be(List(2, 1))
+    }
+
+    it("should return List(2,1,1) for List(1,1,1,2,1,1) when taking 1") {
+      List(1, 1, 1, 2, 1, 1).myDropWhile(_ == 1) should be(List(2, 1, 1))
+    }
+  }
+
+  describe("map") {
+    it("should transform empty list into empty") {
+      Nil.myMap(a => a) should be(Nil)
+    }
+
+    it("should transform List(2) into List(4) for double function") {
+      List(2).myMap(_ * 2) should be(List(4))
+    }
+
+    it("should transform List(2,3) into List(4,6) for double function") {
+      List(2, 3).myMap(_ * 2) should be(List(4, 6))
     }
   }
 }
